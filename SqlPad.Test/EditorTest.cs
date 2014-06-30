ad069d88cb6b8106a04f81bf7c8e58106119dc64
Husqvik <husqvik@hotmail.com>
2014-07-01 00:05:51 +0200

    Added block and line comment tests

--- SqlPad.Test/EditorTest.cs
+++ SqlPad.Test/EditorTest.cs
@@ -50,5 +50,33 @@ namespace SqlPad.Test
 			_editor.Text.ShouldBe("SELECT * FROM SELECTION SELECTION;");
 			_editor.CaretOffset.ShouldBe(33);
 		}
+
+		[Test(Description = @""), STAThread]
+		public void TestBlockComments()
+		{
+			_editor.Text = "SELECT * FROM SELECTION;\nSELECT * FROM RESPONDENTBUCKET";
+			_editor.CaretOffset = 13;
+			_editor.SelectionLength = 35;
+
+			GenericCommandHandler.HandleBlockComments(_editor.TextArea, null);
+
+			_editor.Text.ShouldBe("SELECT * FROM/* SELECTION;\nSELECT * FROM RESPONDEN*/TBUCKET");
+			_editor.CaretOffset.ShouldBe(50);
+			_editor.SelectionLength.ShouldBe(0);
+		}
+
+		[Test(Description = @""), STAThread]
+		public void TestLineComments()
+		{
+			_editor.Text = "SELECT * FROM SELECTION;\nSELECT * FROM RESPONDENTBUCKET";
+			_editor.CaretOffset = 13;
+			_editor.SelectionLength = 35;
+
+			GenericCommandHandler.HandleLineComments(_editor.TextArea, null);
+
+			_editor.Text.ShouldBe("--SELECT * FROM SELECTION;\n--SELECT * FROM RESPONDENTBUCKET");
+			_editor.CaretOffset.ShouldBe(52);
+			_editor.SelectionLength.ShouldBe(0);
+		}
 	}
 }
---
+++ SqlPad.Test/VisualComponentTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using NUnit.Framework;
+
+namespace SqlPad.Test
+{
+	[TestFixture]
+	public class VisualComponentTest
+	{
+		[Test(Description = @""), STAThread, RequiresThread]
+		public void TestMainWindowInitialization()
+		{
+			var mainWindow = new MainWindow();
+			mainWindow.Show();
+			var page = (DocumentPage)((TabItem)mainWindow.DocumentTabControl.SelectedItem).Content;
+			page.IsParsingSynchronous = true;
+			mainWindow.Show();
+			page.Editor.Text = "SELECT * FROM DUAL";
+			mainWindow.Close();
+			Dispatcher.CurrentDispatcher.InvokeShutdown();
+		}
+	}
+}
