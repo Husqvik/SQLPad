ad1dd20d0e455ef6dc4c7d9b437924f3819fdfc8
Husqvik <husqvik@hotmail.com>
2014-07-14 10:55:11 +0200

    Bugfix: Application crashed when attempting to show tooltip on semicolon between statements
    SELECT * FROM DUAL;SELECT * FROM DUAL

--- SqlPad/StatementCollection.cs
+++ SqlPad/StatementCollection.cs
@@ -14,7 +14,7 @@ namespace SqlPad
 
 		public StatementBase GetStatementAtPosition(int position)
 		{
-			return Items.SingleOrDefault(s => s.SourcePosition.IndexStart <= position && s.SourcePosition.IndexEnd + 1 >= position);
+			return Items.LastOrDefault(s => s.SourcePosition.IndexStart <= position && s.SourcePosition.IndexEnd + 1 >= position);
 		}
 
 		public StatementDescriptionNode GetNodeAtPosition(int position, Func<StatementDescriptionNode, bool> filter = null)
