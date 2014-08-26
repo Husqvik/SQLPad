76f6401e287a1401bef675681c4b040bcf6b04c7
Husqvik <husqvik@hotmail.com>
2014-08-26 13:46:30 +0200

    Improved: Command execution related commands can be invoked from bind variable list

--- SqlPad/Commands/GenericCommands.cs
+++ SqlPad/Commands/GenericCommands.cs
@@ -10,7 +10,7 @@ namespace SqlPad.Commands
 		public static RoutedCommand CloseDocumentCommand = new RoutedCommand();
 		public static RoutedCommand CloseAllDocumentsButThisCommand = new RoutedCommand();
 		public static RoutedCommand OpenContainingFolderCommand = new RoutedCommand();
-		public static RoutedCommand ExplainPlan = new RoutedCommand("ExplainPlan", typeof(TextEditor), new InputGestureCollection { new KeyGesture(Key.E, ModifierKeys.Control) });
+		public static RoutedCommand ExplainPlan = new RoutedCommand("ExplainPlan", typeof(Grid), new InputGestureCollection { new KeyGesture(Key.E, ModifierKeys.Control) });
 		public static RoutedCommand ExportToCsv = new RoutedCommand();
 		public static RoutedCommand GoToNextEdit = new RoutedCommand("GoToNextEdit", typeof(Window), new InputGestureCollection { new KeyGesture(Key.OemMinus, ModifierKeys.Control | ModifierKeys.Shift) });
 		public static RoutedCommand GoToPreviousEdit = new RoutedCommand("GoToPreviousEdit", typeof(Window), new InputGestureCollection { new KeyGesture(Key.OemMinus, ModifierKeys.Control) });
@@ -25,8 +25,8 @@ namespace SqlPad.Commands
 		public static RoutedCommand NavigateToNextUsageCommand = new RoutedCommand("NavigateToNextHighlightedUsage", typeof(TextEditor), new InputGestureCollection { new KeyGesture(Key.PageDown, ModifierKeys.Control | ModifierKeys.Alt) });
 		public static RoutedCommand NavigateToQueryBlockRootCommand = new RoutedCommand("NavigateToQueryBlockRoot", typeof(TextEditor), new InputGestureCollection { new KeyGesture(Key.Home, ModifierKeys.Control | ModifierKeys.Alt) });
 		public static RoutedCommand NavigateToDefinitionRootCommand = new RoutedCommand("NavigateToDefinition", typeof(TextEditor), new InputGestureCollection { new KeyGesture(Key.F12) });
-		public static RoutedCommand ExecuteDatabaseCommandCommand = new RoutedCommand("ExecuteDatabaseCommand", typeof(TextEditor), new InputGestureCollection { new KeyGesture(Key.F9), new KeyGesture(Key.Enter, ModifierKeys.Control) });
-		public static RoutedCommand ExecuteDatabaseCommandWithActualExecutionPlanCommand = new RoutedCommand("ExecuteDatabaseCommandWithActualExecutionPlan", typeof(TextEditor), new InputGestureCollection { new KeyGesture(Key.F9, ModifierKeys.Shift), new KeyGesture(Key.Enter, ModifierKeys.Control | ModifierKeys.Shift) });
+		public static RoutedCommand ExecuteDatabaseCommandCommand = new RoutedCommand("ExecuteDatabaseCommand", typeof(Grid), new InputGestureCollection { new KeyGesture(Key.F9), new KeyGesture(Key.Enter, ModifierKeys.Control) });
+		public static RoutedCommand ExecuteDatabaseCommandWithActualExecutionPlanCommand = new RoutedCommand("ExecuteDatabaseCommandWithActualExecutionPlan", typeof(Grid), new InputGestureCollection { new KeyGesture(Key.F9, ModifierKeys.Shift), new KeyGesture(Key.Enter, ModifierKeys.Control | ModifierKeys.Shift) });
 		public static RoutedCommand SaveCommand = new RoutedCommand("Save", typeof(TextEditor), new InputGestureCollection { new KeyGesture(Key.S, ModifierKeys.Control) });
 		public static RoutedCommand SaveAsCommand = new RoutedCommand("SaveAs", typeof(TextEditor));
 		public static RoutedCommand SaveAllCommand = new RoutedCommand("SaveAs", typeof(Window), new InputGestureCollection { new KeyGesture(Key.S, ModifierKeys.Control | ModifierKeys.Shift) });
@@ -34,7 +34,7 @@ namespace SqlPad.Commands
 		public static RoutedCommand FormatStatementAsSingleLineCommand = new RoutedCommand("FormatStatementAsSingleLine", typeof(TextEditor), new InputGestureCollection { new KeyGesture(Key.F, ModifierKeys.Shift | ModifierKeys.Alt) });
 		public static RoutedCommand FindUsagesCommand = new RoutedCommand("FindUsages", typeof(TextEditor), new InputGestureCollection { new KeyGesture(Key.F11, ModifierKeys.Alt | ModifierKeys.Shift) });
 		public static RoutedCommand FetchNextRowsCommand = new RoutedCommand("FetchNextRows", typeof(DataGrid), new InputGestureCollection { new KeyGesture(Key.PageDown), new KeyGesture(Key.Down) });
-		public static RoutedCommand CancelStatementCommand = new RoutedCommand("CancelStatement", typeof(TextEditor), new InputGestureCollection { new KeyGesture(Key.Escape) });
+		public static RoutedCommand CancelStatementCommand = new RoutedCommand("CancelStatement", typeof(Grid), new InputGestureCollection { new KeyGesture(Key.Escape) });
 		public static RoutedCommand RefreshDatabaseModelCommand = new RoutedCommand("RefreshDatabaseModel", typeof(TextEditor), new InputGestureCollection { new KeyGesture(Key.F5) });
 	}
 }
