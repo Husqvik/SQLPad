cd896f7ff9e3e766f4c6ed3bf3f9ea7ed3fb020a
Husqvik <husqvik@hotmail.com>
2014-11-15 18:27:06 +0100

    Maintenance: AvalonEdit updated to new version

--- SqlPad/SqlFoldingStrategy.cs
+++ SqlPad/SqlFoldingStrategy.cs
@@ -1,16 +1,13 @@
-using System.Collections.Generic;
 using System.Linq;
 using ICSharpCode.AvalonEdit;
-using ICSharpCode.AvalonEdit.Document;
 using ICSharpCode.AvalonEdit.Folding;
 
 namespace SqlPad
 {
-	public class SqlFoldingStrategy : AbstractFoldingStrategy
+	public class SqlFoldingStrategy
 	{
 		private readonly FoldingManager _foldingManager;
 		private readonly TextEditor _editor;
-		private StatementCollection _statements;
 
 		public SqlFoldingStrategy(FoldingManager foldingManager, TextEditor editor)
 		{
@@ -18,18 +15,13 @@ namespace SqlPad
 			_editor = editor;
 		}
 
-		public override IEnumerable<NewFolding> CreateNewFoldings(TextDocument document, out int firstErrorOffset)
-		{
-			firstErrorOffset = -1;
-			return _statements.SelectMany(s => s.Sections)
-				.Where(IsMultilineOrNestedSection)
-				.Select(s => new NewFolding(s.FoldingStart, s.FoldingEnd) { Name = s.Placeholder });
-		}
-
 		public void UpdateFoldings(StatementCollection statements)
 		{
-			_statements = statements;
-			UpdateFoldings(_foldingManager, _editor.Document);
+			var foldings = statements.SelectMany(s => s.Sections)
+				.Where(IsMultilineOrNestedSection)
+				.Select(s => new NewFolding(s.FoldingStart, s.FoldingEnd) {Name = s.Placeholder});
+			
+			_foldingManager.UpdateFoldings(foldings, -1);
 		}
 
 		public void Store(WorkingDocument workingDocument)
