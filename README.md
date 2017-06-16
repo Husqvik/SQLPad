2fe1d466bbdb62abc880c779974459e1388b4ce0
Husqvik <husqvik@hotmail.com>
2017-06-16 22:10:00 +0200

    update references

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SQLPad 0.4.0.472
+# SQLPad 0.4.0.473
 
 |Builds|Oracle.DataAccess|Oracle.ManagedDataAccess|
 |:--:|:--:|:--:|
--- SqlPad.Oracle/Properties/AssemblyInfo.cs
+++ SqlPad.Oracle/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using SqlPad;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.0.472")]
-[assembly: AssemblyFileVersion("0.4.0.472")]
-[assembly: AssemblyBuildInfo("2b3cd898", "2017-06-05 15:56:44")]
+[assembly: AssemblyVersion("0.4.0.473")]
+[assembly: AssemblyFileVersion("0.4.0.473")]
+[assembly: AssemblyBuildInfo("a618e55a", "2017-06-16 22:09:25")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
